c83a1284eec35a2bad96e6f7f14fa876530cfc32
wuhuanhost <post.thinking@gmail.com>
2016-08-26 12:20:11 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use script';
 
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var Server = require('karma').Server;
 
 
 // 默认任务
@@ -9,11 +11,25 @@ gulp.task('default', function () {
 
 });
 
-// bulid任务
-gulp.task('build', function () {
-
-
+// 清空dist目录
+gulp.task('clean', function () {
+    gulp.src(__dirname + '/dist/', { read: false })
+        .pipe(clean());
 });
+
+
+
+
+/**
+ * 运行测试
+ */
+gulp.task('test', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
 
 
 // dev任务
@@ -25,9 +41,9 @@ gulp.task('build', function () {
 
 // 文件监控
 gulp.watch('./webapp/**/*.js', ['js'])
-  .on('change', function (event) {
-    console.log(event.path + "------js文件发生变化");
-  });
+    .on('change', function (event) {
+        console.log(event.path + "------js文件发生变化");
+    });
 
 
 
